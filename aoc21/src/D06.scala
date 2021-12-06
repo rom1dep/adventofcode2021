6d0ad20076253126936631b20ee5fc344787ab92
Romain DEP. <rom1dep@gmail.com>
2021-12-07 00:28:15 +0100

    day 06 enh

--- aoc21/src/D06.scala
+++ aoc21/src/D06.scala
@@ -6,9 +6,9 @@ object D06 extends Problem(2021, 6):
 
     part1((1 to 80).foldLeft(fishes)((prevFishes, _) => prevFishes.flatMap(t => if t == 0 then Array(6, 8) else Array(t - 1))).length)
     part2((1 to 256).foldLeft(fishes.groupMapReduce(identity)(_ => 1L)(_ + _))((prevFishes: Map[Int, Long], _) =>
-      (for (k, v) <- prevFishes
-        newK = if k == 0 then 6 else k - 1
-        newV = if newK == 6 then prevFishes.getOrElse(0, 0L) + prevFishes.getOrElse(7, 0L) else v
-      yield newK -> newV).toMap
-        .updated(8, prevFishes.getOrElse(0, 0L))
+      prevFishes.map((k, v) =>
+        val newK = if k == 0 then 6 else k - 1
+        val newV = if newK == 6 then prevFishes.getOrElse(0, 0L) + prevFishes.getOrElse(7, 0L) else v
+        (newK, newV)
+      ).updated(8, prevFishes.getOrElse(0, 0L))
     ).values.sum)
